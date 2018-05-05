fbf9fd61c46fe94ac6fb40aa7e4c49372bf8f364
jasonduquain <jduquain@gmail.com>
2018-05-05 08:26:51 -0400

    reveal on scroll and back to top btn added

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -24,7 +24,31 @@ var mobileMenu = new MobileMenu();
 
 /******** RevealOnScroll.js********/
 
+class RevealOnScroll {
+    constructor() {
+        this.itemsToReveal = document.querySelectorAll('.feature-item, .testimonial');
+        this.hideInitially();
+        this.showElements();
+    }
+    
+    hideInitially() {
+       this.itemsToReveal.forEach((el) => {
+           el.classList.add('reveal-item');
+       }); 
+    }
+    
+    showElements() {
+       window.addEventListener('scroll', () => {
+           this.itemsToReveal.forEach((el) => {
+               if (el.getBoundingClientRect().bottom <= (window.innerHeight)) {
+                   el.classList.add('reveal-item--is-visible')
+               }
+           }); 
+       })     
+    }
+}
 
+var revealOnScroll = new RevealOnScroll();
 
 
 
@@ -45,3 +69,27 @@ var mobileMenu = new MobileMenu();
 
 
 /*******  BACK TO TOP BTN  ***********/
+
+let bttBtn = document.querySelector('.back-to-top');
+let html = document.documentElement;
+let body = document.body;
+
+let heights = [html.scrollHeight, html.getBoundingClientRect().height];
+
+let maxHeight = Math.max(...heights);
+
+
+window.addEventListener('scroll', () => {
+    if (html.scrollTop >= (maxHeight / 3)) {
+       bttBtn.classList.add('back-to-top--active'); 
+    } else {
+       bttBtn.classList.remove('back-to-top--active');  
+    }
+});
+
+bttBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    html.scrollTop = 0;
+});
+
+
